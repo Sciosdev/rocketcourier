61150c46cb01e6ca1d09c7c9019855381d4babb2
Sciosdev <167479188+Sciosdev@users.noreply.github.com>
2025-06-30 05:53:41 -0600

    fix permisos de configuracion

--- frontend/src/app/pages/pages-menu.ts
+++ frontend/src/app/pages/pages-menu.ts
@@ -38,6 +38,15 @@ export const admin_menu: NbMenuItem[] = [
     },
   },
   {
+    title: 'Configuración',
+    icon: 'settings-outline',
+    link: '/intranet/configuracion',
+    data: {
+      permission: 'menu',
+      resource: ['customer']
+    },
+  },
+  {
     title: 'Administración',
     icon: 'settings-2-outline',
     data: {
